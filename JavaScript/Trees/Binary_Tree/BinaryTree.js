8d0d95afc96d2b958d8799346b037a6c29978e94
halls7588 <halls7588@gmail.com>
2016-07-06 16:44:17 -0700

    yep

--- JavaScript/Trees/Binary_Tree/BinaryTree.js
+++ JavaScript/Trees/Binary_Tree/BinaryTree.js
@@ -1,5 +1,6 @@
 
 function Node(data){
+    this.parent = null;
     this.left = null;
     this.right = null;
     this.data = data; 
@@ -7,42 +8,118 @@ function Node(data){
 
 function BinaryTree(){
     this.root = null;
+    
+    var compare = function(data, node){
+        if(node == null || data == null)
+            return null;
+        
+        if(node.data > data){
+            return 1;
+        }
+        
+        if(node.data < data){
+            return -1;
+        }
+        
+        if(node.data == data){
+            return 0;
+        }
+        return null;
+    };
+    
+    var insertHelper = function(data, node){
+        var cmp = this.compare(data, node);
+        if(cmp !- null){
+            if(cmp == 1){
+                if(!node.right){
+                    newNode = new Node(data);
+                    node.right = newNode;
+                    newNode.parent = node;
+                    return newNode;
+                }
+                else
+                    return insertHelper(data, node.right);
+            }
+            if(cmp <= 0){
+                if(!node.left){
+                    newNode = new Node(data);
+                    node.left = newNode;
+                    newNode.parent = node;
+                    return newNode;
+                }
+                else
+                    return insertHelper(data, node.left);
+            }
+        }
+        else
+            return null;
+    };
+    
+    var removeHelper = function(data, node){
+        var cmp = this.compare(data, node);
+        if(cmp !- null){
+            if(cmp == 1)
+                return removeHelper(data, node.right);
+            if(cmp == -1)
+                return removeHelper(data, node.left);
+            if(cmp == 0){
+                //has no children
+                if(!node.left && !node.right){
+                    pnode = node.parent;
+                    if(pnode.left)
+                }
+                //has one child
+                
+                //has 2 children
+                
+            }
+
+        }
+        else
+            return null;
+    };
+    
 }
 
 BinaryTree.prototype.insert = function(data){
-    var node = new Node(data);
-
-   if(!root){
-      this.root = node;
-      return;
-   }
-   var tmp = this.root;
-
-   while(tmp){
-      if(data < tmp.data){
-          if(!tmp.left){
-             tmp.left = node;
-             break;
-          }
-          else{
-             tmp = tmp.left;
-        }
-     }
-     else{
-         if(!tmp.right){
-            tmp.right = node;
-            break;
-         }
-         else{
-            tmp = tmp.right;
-         }
-     }
-  }
-
-
-BinaryTree.prototype.remove = function (value) {
-    this.root = this.remove(value, this.root);
+    if(!this.root){
+       return this.root = new Node(data);
+    }
+    return insertHelper(data, this.root);
 };
+
+
+BinaryTree.prototype.remove = function (data) {
+    if(!this.root)
+        return null;
+    return this.removeHelper(data, this.root);
+
+};
+    
+BinaryTree.prototype.getMin = function(){
+    if(!this.root)
+        return null;
+    
+    node = this.root;
+    
+    while(nide.left)
+        node = node.left;
+    
+    return node;
+};
+    
+BinaryTree.prototype.getMax = function(){
+    if(!this.root)
+        return null;
+    
+    node = this.root;
+    
+    while(nide.right)
+        node = node.right;
+    
+    return node;
+};
+
 
 BinaryTree.prototype.preOrderTraversal = function(node){
     if(node !== null){
@@ -50,7 +127,7 @@ BinaryTree.prototype.preOrderTraversal = function(node){
         preOrderTraversal(node.left);
         preOrderTraversal(node.right);
     }
-}
+};
 
 BinaryTree.prototype.postOrderTraversal = function(node){
     if(node != null){
@@ -58,7 +135,7 @@ BinaryTree.prototype.postOrderTraversal = function(node){
         postOrderTraversal(node.right);
         console.log(node.data);
     }
-}
+};
 
 BinaryTree.prototype.inOrderTraversal = function(data){
     if(node != null){
@@ -66,7 +143,7 @@ BinaryTree.prototype.inOrderTraversal = function(data){
         console.log(node.data);
         inOrderTraversal(node.right);
     }
-}
+};
 
 BinaryTree.prototype.depthFirstSearch = function(node){
 		if(node == null)
@@ -82,7 +159,7 @@ BinaryTree.prototype.depthFirstSearch = function(node){
 			if(x.left!=null) 
                 stack.push(node.left);			
 		}
-}
+};
 	
 
 BinaryTree.prototype.breadthFirstSearch = function(node){
@@ -100,4 +177,4 @@ BinaryTree.prototype.breadthFirstSearch = function(node){
             queue.push(node.right);
 
     }
-}
+};
