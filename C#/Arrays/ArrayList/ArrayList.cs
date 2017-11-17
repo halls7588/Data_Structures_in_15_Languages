8aed4125c209f38bb27b144cd816ed656de00fb6
Stephen Hall <halls7588@users.noreply.github.com>
2017-11-17 13:37:09 -0800

    Update ArrayList.cs

--- C#/Arrays/ArrayList/ArrayList.cs
+++ C#/Arrays/ArrayList/ArrayList.cs
@@ -1,6 +1,6 @@
 ﻿/*******************************************************
  *  ArrayList.cs
- *  Created by Stephen Hall on 11/20/17.
+ *  Created by Stephen Hall on 11/17/17.
  *  Copyright (c) 2017 Stephen Hall. All rights reserved.
  *  ArrayList implementation in C#
  ********************************************************/
