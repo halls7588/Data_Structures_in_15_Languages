a2e29a53f0b2a10efe05b6abe29d0d3d020d5e1a
Stephen Hall <halls7588@users.noreply.github.com>
2017-11-20 11:15:48 -0800

    Update LinkedStack.cs

--- C#/Stacks/Linked_Stack/LinkedStack.cs
+++ C#/Stacks/Linked_Stack/LinkedStack.cs
@@ -1,72 +1,55 @@
-﻿/*******************************************************
+/*******************************************************
  *  LinkedStack.cs
  *  Created by Stephen Hall on 9/25/17.
  *  Copyright (c) 2017 Stephen Hall. All rights reserved.
  *  A Linked Stack implementation in C#
  ********************************************************/
 
-namespace DataStructures
+namespace DataStructures.Stacks.LinkedStack
 {
     /// <summary>
     /// Linked Stack Class
     /// </summary>
-    /// <typeparam name="T">Generic Type</typeparam>
+    /// <typeparam name="T"> Generic Type</typeparam>
     public class LinkedStack<T>
     {
         /// <summary>
-        /// Node Class For Linked Stack
+        /// Node Class for Linked Stack
         /// </summary>
-        /// <typeparam name="T">Generic Type</typeparam>
-        public class Node<T>
+        public class Node
         {
             /// <summary>
-            /// Private members of the Node Class
+            /// public accessors for Node class
             /// </summary>
-            private T data;
-            private Node<T> next;
+            public T Data { set; get; }
+
+            public Node Next { set; get; }
 
             /// <summary>
-            /// Public Property for the Node data Member
+            /// Node Constructor
             /// </summary>
-            public T Data
-            {
-                set { data = value; }
-                get { return data; }
-            }
-
-            /// <summary>
-            /// Public property for the Node Next Member
-            /// </summary>
-            public Node<T> Next
-            {
-                set { next = value; }
-                get { return next; }
-            }
-
-            /// <summary>
-            /// ode Class Constructor
-            /// </summary>
-            /// <param name="data">Data to be held in the node</param>
+            /// <param name="data">Data for the node to hold</param>
             public Node(T data)
             {
-                this.data = data;
-                next = null;
+                Data = data;
+                Next = null;
             }
         }
 
         /// <summary>
-        /// Private members of the Linked Stack Class
+        /// Private Members
         /// </summary>
-        private int count;
-        private Node<T> head;
+        private int _count;
+
+        private Node _head;
 
         /// <summary>
-        /// Linked Stack class constructor
+        /// Linked Stack Constructor
         /// </summary>
         public LinkedStack()
         {
-            count = 0;
-            head = null;
+            _count = 0;
+            _head = null;
         }
 
         /// <summary>
@@ -74,12 +57,12 @@ namespace DataStructures
         /// </summary>
         /// <param name="data">Data to be added to the stack</param>
         /// <returns>Node added to the stack</returns>
-        public Node<T> Push(T data)
+        public Node Push(T data)
         {
-            Node<T> node = new Node<T>(data);
-            node.Next = head;
-            head = node;
-            count++;
+            Node node = new Node(data);
+            node.Next = _head;
+            _head = node;
+            _count++;
             return Top();
         }
 
@@ -87,40 +70,31 @@ namespace DataStructures
         /// Pops item off the stack
         /// </summary>
         /// <returns>Node popped off of the stack</returns>
-        public Node<T> Pop()
+        public Node Pop()
         {
-            Node<T> node = head;
-            head = head.Next;
+            Node node = _head;
+            _head = _head.Next;
             node.Next = null;
-            count--;
+            _count--;
             return node;
         }
 
         /// <summary>
-        /// Gets the first Node onto of the stack without removing it
+        /// Gets the Node onto of the stack
         /// </summary>
         /// <returns>Node on top of the stack</returns>
-        public Node<T> Top()
-        {
-            return head;
-        }
+        public Node Top() => _head;
 
         /// <summary>
         /// Returns a value indicating if the stack is empty
         /// </summary>
-        /// <returns>True if empty, false if not</returns>
-        public bool IsEmpty()
-        {
-            return (count == 0);
-        }
+        /// <returns>true if empty, false if not</returns>
+        public bool IsEmpty() => _count == 0;
 
         /// <summary>
         /// Returns a value indicating if the stack is full
         /// </summary>
         /// <returns>False, Linked stack is never full</returns>
-        public bool IsFull()
-        {
-            return false;
-        }
+        public bool IsFull() => false;
     }
 }
