99082855b4af8bdbd91527d2408d24e6365e8df1
Stephen Hall <halls7588@users.noreply.github.com>
2017-11-20 11:09:22 -0800

    Update ArrayedStack.cs

--- C#/Stacks/Arrayed_Stack/ArrayedStack.cs
+++ C#/Stacks/Arrayed_Stack/ArrayedStack.cs
@@ -1,97 +1,87 @@
-﻿/*******************************************************
+/*******************************************************
  *  ArrayedStack.cs
  *  Created by Stephen Hall on 9/25/17.
  *  Copyright (c) 2017 Stephen Hall. All rights reserved.
  *  A Arrayed Stack implementation in C#
  ********************************************************/
 
-namespace DataStructures
+namespace DataStructures.Stacks.ArrayedStack
 {
     /// <summary>
-    /// Arrayed Stack Class
+    ///  Arrayed Stack Class
     /// </summary>
-    /// <typeparam name="T">Generic Type</typeparam>
+    /// <typeparam name="T"> Generic type</typeparam>
     public class ArrayedStack<T>
     {
         /// <summary>
-        /// Private members
+        /// Private Members
         /// </summary>
-        private T[] array;
-        private int count;
-        private int size;
+        private T[] _array;
+        private int _count;
+        private int _size;
 
         /// <summary>
         /// Default Constructor
         /// </summary>
         public ArrayedStack()
         {
-            array = new T[(size = 10)];
-            count = 0;
+            _array = new T[_size = 10];
+            _count = 0;
         }
-       
+
         /// <summary>
         /// Arrayed Stack Constructor
         /// </summary>
         /// <param name="size">Size to initialize the stack to</param>
         public ArrayedStack(int size)
         {
-            array = new T[(this.size = size)];
-            count = 0;
+            _array = new T[_size = size];
+            _count = 0;
         }
-        
+
         /// <summary>
         /// Pushes given data onto the stack if space is available
         /// </summary>
         /// <param name="data">Data to be added to the stack</param>
-        /// <returns>Node added to the stack</returns>
+        /// <returns>Item added to the stack</returns>
         public T Push(T data)
         {
             if (!IsFull())
             {
-                array[count] = data;
-                count++;
+                _array[_count] = data;
+                _count++;
                 return Top();
             }
             return default(T);
         }
-        
+
         /// <summary>
-        /// Pops the first item off the stack
+        /// Pops item off the stack
         /// </summary>
-        /// <returns>Node popped off of the stack</returns>
+        /// <returns>Item popped off of the stack</returns>
         public T Pop()
         {
-            T data = array[count - 1];
-            count--;
+            T data = _array[_count - 1];
+            _count--;
             return data;
         }
-        
+
         /// <summary>
-        /// Gets the Node onto of the stack without removing it
+        /// Gets the Item onto of the stack
         /// </summary>
-        /// <returns>Node on top of the stack</returns>
-        public T Top()
-        {
-            return array[count - 1];
-        }
-       
+        /// <returns>Item on top of the stack</returns>
+        public T Top() =>  _array[_count - 1];
+
         /// <summary>
         /// Returns a value indicating if the stack is empty
         /// </summary>
-        /// <returns>True if empty, false if not</returns>
-        public bool IsEmpty()
-        {
-            return (count == 0);
-        }
+        /// <returns>true if empty, false if not</returns>
+        public bool IsEmpty() => _count == 0;
 
         /// <summary>
         /// Returns a value indicating if the stack is full
         /// </summary>
         /// <returns>True if full, false if not</returns>
-        public bool IsFull()
-        {
-            return (count == size);
-        }
+        public bool IsFull() => _count == _size;
     }
-
 }
