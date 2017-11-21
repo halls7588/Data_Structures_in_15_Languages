2aafe286b738835df8846c9ad9f6529f71024b6f
Stephen Hall <halls7588@users.noreply.github.com>
2017-11-21 05:59:30 -0800

    Update ArrayedQueue.vb

--- Visual_Basic/Queues/Arrayed_Queue/ArrayedQueue.vb
+++ Visual_Basic/Queues/Arrayed_Queue/ArrayedQueue.vb
@@ -1,99 +1,96 @@
-﻿'******************************************************
+'******************************************************
 ' *  ArrayedQueue.vb
 ' *  Created by Stephen Hall on 11/01/17.
 ' *  Copyright (c) 2017 Stephen Hall. All rights reserved.
-' *  A Arrayed Queue implementation in Visual Basic
+' *  A Arrayed Queue implementation in Visula Basic
 ' *******************************************************
 
-
-Namespace Data_Structures
-    Class ArrayedQueue(Of T)
-        ''' <summary>
-        ''' Private members
+Namespace Queues.ArrayedQueue
+	''' <summary>
+	''' Arrayed Queue Class
+	''' </summary>
+	''' <typeparam name="T">Generic type</typeparam>
+	Public Class ArrayedQueue(Of T As IComparable)
+		''' <summary>
+        ''' Private Members  
         ''' </summary>
-        Private array As T()
-        Private count As Integer
-        Private size As Integer
+		Private _array As T()
+		Private _count As Integer
+		Private _size As Integer
+
+		''' <summary>
+		''' Default Constructor
+		''' </summary>
+		Public Sub New()
+			_array = New T(InlineAssignHelper(_size, 10) - 1) {}
+			_count = 0
+		End Sub
+
+		''' <summary>
+		''' Arrayed Queue Constructor
+		''' </summary>
+		''' <param name="size">Size to initialize the queue to</param>
+		Public Sub New(size As Integer)
+			_array = New T(InlineAssignHelper(_size, size) - 1) {}
+			_count = 0
+		End Sub
 
         ''' <summary>
-        ''' Default Constructor
-        ''' </summary>
-        Public Sub New()
-            array = New T((InlineAssignHelper(size, 10)) - 1) {}
-            count = 0
-        End Sub
-
-        ''' <summary>
-        ''' Arrayed queue Constructor
-        ''' </summary>
-        ''' <param name="size">Size to initialize the queue to</param>
-        Public Sub New(size As Integer)
-            array = New T((InlineAssignHelper(Me.size, size)) - 1) {}
-            count = 0
-        End Sub
-
-        ''' <summary>
-        ''' Add given data onto the queue if space is available
+        ''' Pushes given data onto the queue if space is available
         ''' </summary>
         ''' <param name="data">Data to be added to the queue</param>
-        ''' <returns>item added to the queue</returns>
-        Public Function Enqueue(data As T) As T
-            If Not IsFull() Then
-                array(count) = data
-                count += 1
-                Return Top()
-            End If
-            Return Nothing
-        End Function
+        ''' <returns>Item added to the queue</returns>
+		Public Function Enqueue(data As T) As T
+			If Not IsFull() Then
+				_array(_count) = data
+				_count += 1
+				Return Top()
+			End If
+			Return Nothing
+		End Function
 
-        ''' <summary>
-        ''' Removed the first item off the queue
-        ''' </summary>
-        ''' <returns>item removed off of the queue</returns>
-        Public Function Dequeue() As T
-            If IsEmpty() Then
-                Return Nothing
-            End If
+		''' <summary>
+		''' Removes item from the queue
+		''' </summary>
+		''' <returns>item removed from the queue</returns>
+		Public Function Dequeue() As T
+			If IsEmpty() Then
+				Return Nothing
+			End If
+			Dim data As T = _array(0)
+			Dim tmp As T() = New T(_size - 1) {}
+			Array.Copy(_array, 1, tmp, 0, _size - 1 - 1)
+			_array = tmp
+			_count -= 1
+			Return data
+		End Function
 
-            Dim data As T = array(0)
-            Dim tmp As T() = New T((Me.size) - 1) {}
-            For i As Integer = 1 To size - 2
-                tmp(i - 1) = array(i)
-            Next
-            array = tmp
-            count -= 1
-            Return data
-        End Function
+		''' <summary>
+		''' Gets the top item of the queue
+		''' </summary>
+		''' <returns>item on top of the queue</returns>
+		Public Function Top() As T
+			Return If((IsEmpty()), Nothing, _array(0))
+		End Function
 
-        ''' <summary>
-        ''' Gets the item onto of the queue without removing it
-        ''' </summary>
-        ''' <returns>Node on top of the queue</returns>
-        Public Function Top() As T
-            If IsEmpty() Then
-                Return Nothing
-            End If
-            Return array(0)
-        End Function
+		''' <summary>
+		''' Returns a value indicating if the queue is empty
+		''' </summary>
+		''' <returns>true if empty, false if not</returns>
+		Public Function IsEmpty() As Boolean
+			Return (_count = 0)
+		End Function
 
-        ''' <summary>
-        ''' Returns a value indicating if the queue is empty
-        ''' </summary>
-        ''' <returns>True if empty, false if not</returns>
-        Public Function IsEmpty() As Boolean
-            Return (count = 0)
-        End Function
-
-        ''' <summary>
-        ''' Returns a value indicating if the queue is full
-        ''' </summary>
-        ''' <returns>True if full, false if not</returns>
-        Public Function IsFull() As Boolean
-            Return (count = size)
-        End Function
-        Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, value As T) As T
-            target = value
-            Return value
-        End Function
-    End Class
+		''' <summary>
+		''' Returns a value indicating if the queue is full
+		''' </summary>
+		''' <returns>True if full, false if not</returns>
+		Public Function IsFull() As Boolean
+			Return _count = _size
+		End Function
+		Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, value As T) As T
+			target = value
+			Return value
+		End Function
+	End Class
 End Namespace
